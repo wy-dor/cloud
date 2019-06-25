c480bbd6e4a9595cce3947d35880075c598c64e2
wangyan <wy_dor@163.com>
2019-06-25 20:10:48 +0800

    update after merge

--- src/main/java/com/learning/cloud/user/teacher/dao/TeacherDao.java
+++ src/main/java/com/learning/cloud/user/teacher/dao/TeacherDao.java
@@ -1,5 +1,6 @@
 package com.learning.cloud.user.teacher.dao;
 
+import com.learning.cloud.dept.gradeClass.entity.GradeClass;
 import com.learning.cloud.user.teacher.entity.Teacher;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,7 @@ import java.util.List;
 @Mapper
 public interface TeacherDao {
 
-    List<Teacher> getTeachers();
-
-    Teacher getById(Integer id);
+    List<Teacher> getTeachers(Teacher teacher);
 
     int insert(Teacher record);
 
@@ -20,4 +19,10 @@ public interface TeacherDao {
 
     int update(Teacher teacher);
 
+    Integer getClassTeacherNum(Integer classId);
+
+    List<Teacher> getClassTeachers(GradeClass gradeClass);
+
+    //获取学校的老师
+    List<Teacher> getTeacherIds(Long schoolId);
 }
