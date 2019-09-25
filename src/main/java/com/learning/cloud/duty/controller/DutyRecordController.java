302714f536b60222c2a664ee19229ba598001eca
yuyitao <yuforworkcat@163.com>
2019-09-25 16:16:02 +0800

    修改班级值日检查查询接口，返回值新增检查人姓名。

--- src/main/java/com/learning/cloud/duty/controller/DutyRecordController.java
+++ src/main/java/com/learning/cloud/duty/controller/DutyRecordController.java
@@ -44,7 +44,6 @@ public class DutyRecordController {
 
     // 批量插入记录
     @PostMapping("/addDutyRecordList")
-                                      //@RequestBody List<BatchClass> batchClassList
     public JsonResult addDutyRecordList(@RequestBody List<DutyRecord> dutyRecordList)throws Exception{
         return dutyRecordService.addDutyRecordList(dutyRecordList);
     }
