31777955e0abca341c1e84e279e9784aa9d981f5
wangyan <wy_dor@163.com>
2019-10-23 15:25:32 +0800

    图片保存返回RUL1

--- src/main/java/com/learning/cloud/ding/picture/controller/PictureController.java
+++ src/main/java/com/learning/cloud/ding/picture/controller/PictureController.java
@@ -7,28 +7,14 @@ import com.learning.cloud.ding.picture.service.PictureService;
 import com.learning.cloud.ding.question.service.QuestionService;
 import com.learning.domain.JsonResult;
 import com.learning.utils.JsonResultUtil;
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGEncodeParam;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
-import jdk.nashorn.internal.runtime.logging.Logger;
-import lombok.extern.log4j.Log4j;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import sun.misc.BASE64Decoder;
-import sun.rmi.runtime.Log;
 
-import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -44,9 +30,6 @@ public class PictureController {
 
     @Autowired
     private QuestionService questionService;
-
-    @Value("${spring.system.urlPath)")
-    private String urlPath;
 
     @GetMapping("/getPicById")
     public void getPicById(@RequestParam(value="picId") Long picId, HttpServletResponse response)throws Exception{
